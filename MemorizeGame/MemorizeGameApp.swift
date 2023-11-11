b5e2dbfe17a2a8d9f6a58731bfb54eb34409375c
Pridelory <wangmeng.pantera@gmail.com>
2023-11-11 17:20:08 +0000

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by 王蒙 on 11/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
